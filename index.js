d95b387bc40c93e5de1f33bfbfb5b9077a1d6f12
Ankit Saxena <47791661+ankitsaxena21@users.noreply.github.com>
2021-10-01 15:35:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,40 +5,40 @@ const result = document.getElementById('result')
 const apiURL = 'https://api.lyrics.ovh';
 
 
-form.addEventListener('submit', e=> {
+form.addEventListener('submit', e => {
     e.preventDefault();
     searchValue = search.value.trim()
 
-    if(!searchValue){
+    if (!searchValue) {
         alert("There is nothing to search")
     }
-    else{ 
+    else {
         searchSong(searchValue)
     }
 })
 
 
- 
-async function searchSong(searchValue){
+
+async function searchSong(searchValue) {
     const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`)
     const data = await searchResult.json();
 
     showData(data)
 }
 
-function showData(data){
-  
+function showData(data) {
+
     result.innerHTML = `
     <ul class="song-list">
       ${data.data
-        .map(song=> `<li>
+            .map(song => `<li>
                     <div>
                         <strong>${song.artist.name}</strong> -${song.title} 
                     </div>
                     <span data-artist="${song.artist.name}" data-songtitle="${song.title}"> get lyrics</span>
                 </li>`
-        )
-        .join('')}
+            )
+            .join('')}
     </ul>
   `;
 }
@@ -46,13 +46,13 @@ function showData(data){
 
 
 
-result.addEventListener('click', e=>{
+result.addEventListener('click', e => {
     const clickedElement = e.target;
 
-    if (clickedElement.tagName === 'SPAN'){
+    if (clickedElement.tagName === 'SPAN') {
         const artist = clickedElement.getAttribute('data-artist');
         const songTitle = clickedElement.getAttribute('data-songtitle');
-        
+
         getLyrics(artist, songTitle)
     }
 })
@@ -60,10 +60,9 @@ result.addEventListener('click', e=>{
 async function getLyrics(artist, songTitle) {
     const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
     const data = await res.json();
-  
     const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
-  
+
     result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
     <p>${lyrics}</p>`;
-  
-  }
+
+}
